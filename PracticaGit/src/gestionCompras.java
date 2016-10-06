2888815530a9bf4f6edd416a7bfe8ed6a11e09c2
Marleny <Marleny@Marleny-PC>
2016-10-06 21:13:00 +0200

    Nueva version compras

--- PracticaGit/src/gestionCompras.java
+++ PracticaGit/src/gestionCompras.java
@@ -3,7 +3,7 @@ public class gestionCompras {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Gestion Compras");
+		System.out.println("Gestion Compras 1.0");
 	}
 
 }
